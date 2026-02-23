d66e54fe17aba975c9451788b79e2f8488715630
Gerosa Alberto <gerosa.alberto.studente@itispaleocapa.it>
2026-02-23 08:26:36 +0000

    nuovo salvataggio

--- database.sql
+++ database.sql
@@ -34,21 +34,90 @@ CREATE TABLE Catalogo (
 INSERT INTO Fornitori VALUES
 ('F1', 'Acme', 'Roma'),
 ('F2', 'Beta', 'Milano'),
-('F3', 'Gamma', 'Torino');
+('F3', 'Gamma', 'Torino'),
+('F4', 'Delta', 'Napoli'),
+('F5', 'Epsilon', 'Bologna'),
+('F6', 'Zeta', 'Firenze'),
+('F7', 'Eta', 'Genova'),
+('F8', 'Theta', 'Palermo');
 
 INSERT INTO Pezzi VALUES
 ('P1', 'Vite', 'rosso'),
 ('P2', 'Bullone', 'blu'),
-('P3', 'Dado', 'rosso');
+('P3', 'Dado', 'rosso'),
+('P4', 'Rondella', 'verde'),
+('P5', 'Chiodo', 'rosso'),
+('P6', 'Perno', 'blu'),
+('P7', 'Spina', 'verde'),
+('P8', 'Gancio', 'rosso'),
+('P9', 'Molla', 'blu'),
+('P10', 'Anello', 'verde'),
+('P11', 'Clips', 'rosso'),
+('P12', 'Fascetta', 'blu');
 
 INSERT INTO Catalogo VALUES
+-- Acme (F1) - fornisce pezzi rossi e blu
 ('F1', 'P1', 10),
 ('F1', 'P2', 15),
+('F1', 'P3', 9),
+('F1', 'P5', 11),
+('F1', 'P6', 14),
+('F1', 'P8', 12),
+
+-- Beta (F2) - fornisce vari colori
 ('F2', 'P1', 12),
 ('F2', 'P3', 8),
+('F2', 'P4', 10),
+('F2', 'P7', 13),
+('F2', 'P9', 16),
+('F2', 'P10', 11),
+
+-- Gamma (F3) - fornisce tutti i tipi
 ('F3', 'P1', 9),
 ('F3', 'P2', 14),
-('F3', 'P3', 11);
+('F3', 'P3', 11),
+('F3', 'P4', 12),
+('F3', 'P5', 10),
+('F3', 'P6', 15),
+('F3', 'P7', 14),
+('F3', 'P8', 13),
+('F3', 'P9', 17),
+('F3', 'P10', 12),
+('F3', 'P11', 9),
+('F3', 'P12', 16),
+
+-- Delta (F4) - solo pezzi rossi
+('F4', 'P1', 8),
+('F4', 'P3', 7),
+('F4', 'P5', 9),
+('F4', 'P8', 11),
+('F4', 'P11', 8),
+
+-- Epsilon (F5) - solo pezzi verdi
+('F5', 'P4', 11),
+('F5', 'P7', 12),
+('F5', 'P10', 10),
+
+-- Zeta (F6) - pezzi blu e verdi
+('F6', 'P2', 13),
+('F6', 'P4', 9),
+('F6', 'P6', 12),
+('F6', 'P7', 11),
+('F6', 'P9', 15),
+('F6', 'P10', 13),
+('F6', 'P12', 14),
+
+-- Eta (F7) - pochi pezzi rossi
+('F7', 'P1', 11),
+('F7', 'P5', 12),
+
+-- Theta (F8) - mix di colori
+('F8', 'P2', 16),
+('F8', 'P4', 13),
+('F8', 'P6', 17),
+('F8', 'P8', 10),
+('F8', 'P9', 18),
+('F8', 'P11', 10);
 
 /* ===============================
    QUERY
